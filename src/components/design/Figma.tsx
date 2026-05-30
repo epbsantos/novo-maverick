dcae20573ef09c8fd957f6a09ac273e503df7d74
everctba <everson777@gmail.com>
2026-05-29 23:15:57 -0300

    adicionado link para Figma

--- src/components/design/Figma.tsx
+++ src/components/design/Figma.tsx
@@ -4,7 +4,9 @@ function Figma() {
     <>
       <h3>Figma</h3>
       <img src="/img/Figma.jpg" alt="Figma Layout" style={{ width: '300px', maxWidth: '100%' }} />
-
+      <a href="https://www.figma.com/" target="_blank" rel="noopener noreferrer">
+        Figma
+      </a>
 
 
     </>

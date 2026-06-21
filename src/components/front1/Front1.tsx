0a40f0c478d97e8b64abc8fb7b193bdda2ce085b
mlprolo <mauricio.prolo@gmail.com>
2026-06-21 12:30:40 -0300

    Update Front1.tsx

--- src/components/front1/Front1.tsx
+++ src/components/front1/Front1.tsx
@@ -5,22 +5,50 @@ function Front1() {
     <>
 
         <h3>
-          Desenvolvimento <i>Web Front-End</i> 1
-        </h3>
+        Desenvolvimento <i>Web Front-End</i> 1
+      </h3>
 
-        <h4>Ementa</h4>
-        <p>
-          Linguagens de marcação moderna para estruturação de conteúdo para Web.
-          Linguagem de marcação moderna para estilização de conteúdo na Web. Boas
-          práticas e padrões de apresentação, acessibilidade e responsividade dos
-          conteúdos Web.
-        </p>
+      <h4>Ementa</h4>
+      <p>
+        Linguagens de marcação moderna para estruturação de conteúdo para Web. Linguagem de marcação moderna para estilização de conteúdo na Web.
+        Boas práticas e padrões de apresentação, acessibilidade e responsividade dos conteúdos Web.
+      </p>
+      <h4>Duração</h4>
+      <p>60h</p>
 
-        <h4>Duração</h4>
-        <p>60h</p>
+      <h4>Objetivos de Aprendizagem</h4>
+      <ul>
+        <li>Compreender os fundamentos da linguagem JavaScript e sua aplicação no desenvolvimento web.</li>
+        <li>Compreender a introdução à Web e os conceitos fundamentais para a criação de páginas estáticas na internet.</li>
+        <li>Manipular linguagens de marcação moderna (HTML) para estruturar elementos de texto, mídias, tabelas e formulários.</li>
+        <li>Desenvolver a estilização de conteúdos utilizando folhas de estilo (CSS) para controlar cores, fontes, bordas, espaçamentos e posicionamentos.</li>
+        <li>Aplicar padrões recomendados e boas práticas de apresentação, visando a construção de layouts adaptativos e responsivos para múltiplos dispositivos.</li>
+        <li>Garantir a qualidade e conformidade das páginas por meio de padrões de acessibilidade e técnicas de validação do documento.</li>
+      </ul>
 
-        <a href="https://tsi.td.utfpr.edu.br/sites/default/files/2023-08/PPC_TSI-TD_2022_v1.pdf" target="_blank">Referência do conteúdo - PPC TSI</a>
+      <h4>Conteúdos:</h4>
+      <ul>
+        <li>
+          <strong>HTML (Estruturação):</strong> Introdução à Web e criação de páginas estáticas. 
+          Linguagem de Marcação de Hipertexto HTML, marcações (tags), padrões e atributos. 
+          Parágrafos, negrito, itálico, alinhamentos, links, adição de imagens, vídeo, áudio, formulários, tabelas e divisões.
+        </li>
+        <li>
+          <strong>CSS (Estilização):</strong> Folha de estilo (CSS), seletores, formatação de texto, cores, fundo, bordas, 
+          espaçamentos, alinhamentos, largura, altura e técnicas de posicionamento de elementos.
+        </li>
+        <li>
+          <strong>Responsividade e Padrões:</strong> Conceitos e tipos de layouts aplicados a múltiplos dispositivos. 
+          Introdução e utilização de frameworks de apresentação (Front-End) e processos para validação do documento.
+        </li>
+      </ul>
 
+      <h4>Ferramentas e Tecnologias</h4>
+      <ul>
+        <li><strong>HTML</strong>:Linguagem de marcação moderna utilizada para a organização e estruturação lógica de conteúdos Web.</li>
+        <li><strong>CSS</strong>: Tecnologia para estilização visual, controle de apresentação e desenvolvimento de interfaces responsivas.</li>
+        <li><strong>Framework</strong>: Ferramenta e conjunto de padrões empregados para acelerar a criação de interfaces modernas e adaptativas.</li>
+      </ul>
       
     </>
   );

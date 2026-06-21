aa31935768b96f4e03935729385109aa586e407b
mlprolo <mauricio.prolo@gmail.com>
2026-06-21 12:42:44 -0300

    Update Ihc.tsx

--- src/components/ihc/Ihc.tsx
+++ src/components/ihc/Ihc.tsx
@@ -6,10 +6,42 @@ function Ihc() {
 
       <h4>Ementa</h4>
       <p>
-        Design centrado no usuário. Estilos de interface. Projeto e prototipação de interfaceeinteração para múltiplos dispositivos. Definição e métodos para avaliaçãodeusabilidade e acessibilidade.
+        Design centrado no usuário. Estilos de interface. Projeto e prototipação de interfaceeinteração para múltiplos dispositivos.
+        Definição e métodos para avaliaçãodeusabilidade e acessibilidade.
       </p>
       <h4>Duração</h4>
       <p>30h</p>
+
+      <h4>Objetivos de Aprendizagem</h4>
+      <ul>
+        <li>Compreender a teoria inicial de Interação Humano-Computador para facilitar a comunicação entre o homem e a máquina.</li>
+        <li>Dominar a fundamentação teórica e os diferentes estilos de interface para meios digitais.</li>
+        <li>Desenvolver projetos e protótipos de interfaces gráficas e de interação com foco em múltiplos dispositivos.</li>
+        <li>Aplicar cuidados e técnicas específicas para adaptar o design a variados formatos de tela.</li>
+        <li>Avaliar a usabilidade e a acessibilidade de interfaces por meio do planejamento e da aplicação prática de testes.</li>
+      </ul>
+
+      <h4>Conteúdos:</h4>
+      <ul>
+        <li>
+          <strong>Design Centrado no Usuário e Estilos:</strong> Teoria inicial de Interação Humano-Computador e
+          fundamentação dos estilos de interface
+        </li>
+        <li>
+          <strong>Projeto e Prototipação:</strong> Criação de design de interface utilizando ferramenta digital e
+          estudo de cuidados e técnicas para adaptação a múltiplos dispositivos.
+        </li>
+        <li>
+          <strong>Avaliação de Interfaces:</strong> Teoria inicial, conceitos, definições e tipos de testes de usabilidade e acessibilidade.
+          Metodologia de preparação e aplicação prática dos testes de interface.
+        </li>
+      </ul>
+
+      <h4>Ferramentas e Tecnologias</h4>
+      <ul>
+        <li><strong>Métodos de Teste e Avaliação</strong>: Técnicas estruturadas para conduzir avaliações e coletar métricas de usabilidade e acessibilidade com usuários.</li>
+      </ul>
+
     </>
   )
 
